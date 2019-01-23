811fb7d5daabed0b279fb164fd78b291cae4fde0
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-23 21:17:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(array, index_number)
-  if array[index_number] == " "
+  if array[index_number] == " " or ""
     
     return false
   else
